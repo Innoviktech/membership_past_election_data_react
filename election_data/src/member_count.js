8e630af1fe2ab2cf7a0ab17c1b9cb80e0fde9c80
unknown <rasel@innovik.in>
2021-02-17 09:41:09 +0530

    added constituency and have clicking bug in drop down

--- election_data/src/member_count.js
+++ election_data/src/member_count.js
@@ -12,52 +12,93 @@ class Member_count extends React.Component
         super(props);
         this.state = {
             constituency_data : [],
+            panchayat_data : [],
             constituency_result : "",
             constituency_count : [],
+            panchayat_count : [],
             selectedOption: null,
-           
-        }
+            selectedOption1: null,
+            panchayat_drops :"",  
+           }
     }
-
-    
     componentDidMount() {
         axios.get('http://localhost:8000/constituency/')
           .then(res => {
             const constituency_data = res.data;
             this.setState({ constituency_data });
           })
-          
-      }
-      handleChange = selectedOption => {
-            this.setState({ selectedOption});
-            const vars =selectedOption.value;
-            console.log(vars)
-            axios.post('http://localhost:8000/constituency_count/', {
-          key1:vars
+        }
+    constituency_change = selectedOption => {
+        this.setState({ selectedOption:selectedOption.value});
+        const vars =this.state.selectedOption
+        console.log(vars)
+        axios.post('http://localhost:8000/constituency_count/', {
+        key1:vars
         })
         .then((testing) => {
           const constituency_count=testing.data;
             this.setState({ constituency_count });
-        });
-          };
-    render()
+    });
 
-    {
+    this.setState({ selectedOption1:selectedOption.value});
+    const varsone =this.state.selectedOption1
+    console.log(varsone)
+    axios.post('http://localhost:8000/panchayat_drops/', {
+    key1:varsone
+    })
+    .then((testing) => {
+        const panchayat_drops=testing.data;
+        this.setState({ panchayat_drops });
+    });
+    };
+        //   panchayat_change = selectedOption1 => {
+        //     this.setState({ selectedOption1:selectedOption1.value});
+        //     const vars =this.state.selectedOption;
+        //     console.log(vars)
+
+        //     const vars2 =this.state.selectedOption1;
+        //     console.log(vars2)
+        //     axios.post('http://localhost:8000/panchayat_count/', {
+        //   key1:vars,key2:vars2
+        // })
+        // .then((testing) => {
+        //   const panchayat_count=testing.data;
+        //     this.setState({ panchayat_count });
+        // });
+        //   };
+    render(){
+        //console.log(this.state.selectedOption)
+        console.log(this.state.panchayat_drops)
+    
         const { selectedOption } = this.state;
+        const { selectedOption1 } = this.state;
+       
         const tableicons = {
             Filter : forwardRef((props, ref) => <FilterList {...props} ref={ref} />)
         };
-        console.log(this.state.constituency_count)
-        
+        // console.log(this.state.constituency_count)
         return(
            <div>
                <div>
                    <label>Constituency</label>
-                <Select
+<Select
         value={selectedOption}
-        onChange={this.handleChange}
+        onChange={this.constituency_change}
         options={this.state.constituency_data}
 />
+<label>Panchayat</label>
+<Select
+        value={selectedOption1}
+        onChange={this.constituency_change}
+        options={this.state.panchayat_drops}
+/>
+{/* <label>Polling Number</label> */}
+{/* <Select
+        value={selectedOption2}
+        onChange={this.constituency_change}
+        options={this.state.panchayat_drops}
+/> */}
+
 </div>
 <div className="material">
             <MaterialTable
